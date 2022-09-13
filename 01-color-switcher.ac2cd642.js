9a09c6ac224618b759ce92108e3271ea86141f30
IvanRoschin <103255514+IvanRoschin@users.noreply.github.com>
2022-09-13 07:12:27 +0000

    Deploying to gh-pages from @ IvanRoschin/goit-js-hw-09@a403d83e20d4b8020d1b4f8734758fc9e0a3ffee 🚀

---
+++ 01-color-switcher.ac2cd642.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let l=null;e.addEventListener("click",(function(){l=setInterval((()=>{console.log(`Interval with id ${l} has started!`),d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),l&&(e.disabled=!0,t.disabled=!1)})),t.addEventListener("click",(function(){clearInterval(l),console.log(`Interval with id ${l} has stopped!`),e.disabled=!1,t.disabled=!0})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.ac2cd642.js.map
